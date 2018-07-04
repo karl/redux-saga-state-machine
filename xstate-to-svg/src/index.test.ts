912e085b230c16ea590b505df3e4c4a059cd5c2c
Karl O'Keeffe <github@monket.net>
2018-07-04 09:13:22 +0100

    Extract state machine test descriptions to json files

--- xstate-to-svg/src/index.test.ts
+++ xstate-to-svg/src/index.test.ts
@@ -1,137 +1,27 @@
 import * as fs from 'fs';
 import * as mkdirp from 'mkdirp';
+import * as rimraf from 'rimraf';
 import { xstateToSvg } from './index';
 
-const fixtures = [
-  {
-    name: 'simple',
-    description: {
-      key: 'light',
-      initial: 'green',
-      states: {
-        green: {
-          on: {
-            TIMER: 'yellow',
-            POWER_OUTAGE: 'red',
-          },
-        },
-        yellow: {
-          on: {
-            TIMER: 'red',
-            POWER_OUTAGE: 'red',
-          },
-        },
-        red: {
-          on: {
-            TIMER: 'green',
-            POWER_OUTAGE: 'red',
-          },
-        },
-      },
-    },
-  },
-  {
-    name: 'nested',
-    description: {
-      key: 'light',
-      initial: 'green',
-      states: {
-        green: {
-          on: {
-            TIMER: 'yellow',
-          },
-        },
-        yellow: {
-          on: {
-            TIMER: 'red',
-          },
-        },
-        red: {
-          on: {
-            TIMER: 'green',
-          },
-          initial: 'walk',
-          states: {
-            walk: {
-              on: {
-                PED_TIMER: 'wait',
-              },
-            },
-            wait: {
-              on: {
-                PED_TIMER: 'stop',
-              },
-            },
-            stop: {},
-          },
-        },
-      },
-    },
-  },
-  {
-    name: 'parallel',
-    description: {
-      parallel: true,
-      states: {
-        bold: {
-          initial: 'off',
-          states: {
-            on: {
-              on: { TOGGLE_BOLD: 'off' },
-            },
-            off: {
-              on: { TOGGLE_BOLD: 'on' },
-            },
-          },
-        },
-        underline: {
-          initial: 'off',
-          states: {
-            on: {
-              on: { TOGGLE_UNDERLINE: 'off' },
-            },
-            off: {
-              on: { TOGGLE_UNDERLINE: 'on' },
-            },
-          },
-        },
-        italics: {
-          initial: 'off',
-          states: {
-            on: {
-              on: { TOGGLE_ITALICS: 'off' },
-            },
-            off: {
-              on: { TOGGLE_ITALICS: 'on' },
-            },
-          },
-        },
-        list: {
-          initial: 'none',
-          states: {
-            none: {
-              on: { BULLETS: 'bullets', NUMBERS: 'numbers' },
-            },
-            bullets: {
-              on: { NONE: 'none', NUMBERS: 'numbers' },
-            },
-            numbers: {
-              on: { BULLETS: 'bullets', NONE: 'none' },
-            },
-          },
-        },
-      },
-    },
-  },
-];
-
 describe('xstateToSvg', () => {
-  fixtures.forEach((fixture) => {
-    it(fixture.name, () => {
-      const svg = xstateToSvg(fixture.description);
+  const descriptionsDir = __dirname + '/descriptions';
+  const filenames = fs.readdirSync(descriptionsDir);
 
-      mkdirp.sync(__dirname + '/images');
-      fs.writeFileSync(__dirname + '/images/' + fixture.name + '.svg', svg);
+  const imagesDir = __dirname + '/images';
+  rimraf.sync(imagesDir);
+
+  filenames.forEach((filename) => {
+    const description = JSON.parse(
+      fs.readFileSync(descriptionsDir + '/' + filename),
+    );
+
+    const name = filename.split('.')[0];
+
+    it(name, () => {
+      const svg = xstateToSvg(description);
+
+      mkdirp.sync(imagesDir);
+      fs.writeFileSync(imagesDir + '/' + name + '.svg', svg);
 
       expect(svg).toMatchSnapshot();
     });
