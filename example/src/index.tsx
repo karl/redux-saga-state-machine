7b824f10a19a7607ab7bfca2be291ab0ca80aa73
Karl O'Keeffe <github@monket.net>
2018-07-17 21:59:26 +0100

    We have a working state machine!

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,39 +1,131 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import { connect, Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { createStateMachineSaga } from 'redux-saga-state-machine';
 
+const states = {
+  APP: 'APP',
+  PLAYING: 'PLAYING',
+  SWITCHING: 'SWITCHING',
+};
+
+const play = () => {
+  return {
+    type: 'PLAY',
+  };
+};
+const stop = () => {
+  return {
+    type: 'STOP',
+  };
+};
+const next = () => {
+  return {
+    type: 'NEXT',
+  };
+};
+const setCurrentState = (state: string) => {
+  return {
+    type: 'SET_CURRENT_STATE',
+    payload: {
+      state,
+    },
+  };
+};
+
 const initialState = {
-  test: 'yes this is a test',
+  currentState: states.APP,
 };
 
 const reducer = (state = initialState, action: any) => {
-  if (action.type === 'DO_SOMETHING') {
+  if (action.type === 'SET_CURRENT_STATE') {
     return {
       ...state,
-      doSomething: true,
+      currentState: action.payload.state,
     };
   }
   return state;
 };
 
+const selectCurrentState = (state: any) => state.currentState;
+
 const helloSaga = createStateMachineSaga({
   key: 'example-state-machine',
+  setState: setCurrentState,
+  selectState: selectCurrentState,
+  states: {
+    [states.APP]: {
+      on: {
+        ['PLAY']: states.PLAYING,
+      },
+    },
+    [states.PLAYING]: {
+      on: {
+        ['STOP']: states.APP,
+        ['NEXT']: states.SWITCHING,
+      },
+    },
+    [states.SWITCHING]: {
+      on: {
+        ['STOP']: states.APP,
+        ['PLAY']: states.PLAYING,
+      },
+    },
+  },
 });
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(reducer, applyMiddleware(sagaMiddleware));
 sagaMiddleware.run(helloSaga);
 
-// const action = type => store.dispatch({ type });
+const App = ({
+  currentState,
+  onPlay,
+  onStop,
+  onNext,
+}: {
+  currentState: string;
+  onPlay: any;
+  onStop: any;
+  onNext: any;
+}) => {
+  return (
+    <div>
+      <h1>Redux Saga State Machine Example</h1>
 
-const title = 'My Minimal React Webpack Babel Setup';
+      <div>Current State: {currentState}</div>
+
+      <div>
+        <button onClick={onPlay}>Play</button>
+        <button onClick={onStop}>Stop</button>
+        <button onClick={onNext}>Next</button>
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = (state: any) => {
+  return {
+    currentState: selectCurrentState(state),
+  };
+};
+
+const mapDispatchToProps = {
+  onPlay: play,
+  onStop: stop,
+  onNext: next,
+};
+
+const ConnectedApp: any = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(App);
 
 ReactDOM.render(
   <Provider store={store}>
-    <div>{title}</div>
+    <ConnectedApp />
   </Provider>,
   document.getElementById('app'),
 );
--- redux-saga-state-machine/src/index.ts
+++ redux-saga-state-machine/src/index.ts
@@ -1,22 +1,48 @@
+import { SagaIterator } from 'redux-saga';
+import { put, select, take } from 'redux-saga/effects';
 import { Machine } from 'xstate';
-import { MachineConfig } from 'xstate/lib/types';
+
+type ActionCreator = (
+  state: any,
+) => {
+  type: string;
+};
 
 interface IMachineDescription {
   key: string;
+  setState: ActionCreator;
+  selectState: (state: any) => string;
+  states: any;
 }
 
 export const createStateMachineSaga = (description: IMachineDescription) => {
   // tslint:disable-next-line:no-console
   console.log('createStateMachineSaga', description);
 
-  const config: MachineConfig = {
-    states: {},
-  };
+  const { setState, selectState, ...config } = description;
 
   const machine = Machine(config);
 
-  return function*(): IterableIterator<void> {
+  return function*(): SagaIterator {
     // tslint:disable-next-line:no-console
     console.log('running saga', machine);
+
+    while (true) {
+      const state = yield select(selectState);
+      // tslint:disable-next-line:no-console
+      console.log('Current state', state);
+
+      // tslint:disable-next-line:no-console
+      console.log('Events', machine.events);
+      const event = yield take(machine.events);
+      // tslint:disable-next-line:no-console
+      console.log('Event', event);
+
+      const newState = machine.transition(state, event, {});
+      // tslint:disable-next-line:no-console
+      console.log('New state', newState);
+
+      yield put(setState(newState.value));
+    }
   };
 };
