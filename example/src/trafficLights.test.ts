46e2887e2e419159249d37374e8df78fcdd8b10a
Karl O'Keeffe <github@monket.net>
2018-08-01 09:42:33 +0100

    Start work on testing state machine sagas

--- example/src/trafficLights.test.ts
+++ example/src/trafficLights.test.ts
@@ -1,7 +1,20 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { createStateMachineSaga } from 'redux-saga-state-machine';
 import { xstateToSvg } from '../../xstate-to-svg/dist';
-import { stateMachine } from './trafficLights';
+import {
+  actions,
+  reducer,
+  reducerKey,
+  selectors,
+  stateMachine,
+  states,
+} from './trafficLights';
+
+const wait = (duration) =>
+  new Promise((resolve) => setTimeout(() => resolve(), duration));
 
 describe('state machine', () => {
   it('generate visualisation', () => {
@@ -10,4 +23,34 @@ describe('state machine', () => {
       encoding: 'utf8',
     });
   });
+
+  it(
+    'runs saga',
+    async () => {
+      const sagaMiddleware = createSagaMiddleware();
+      const store = createStore(
+        combineReducers({ [reducerKey]: reducer }),
+        undefined,
+        applyMiddleware(sagaMiddleware),
+      );
+
+      const saga = createStateMachineSaga(stateMachine);
+
+      sagaMiddleware.run(saga, {
+        getState: store.getState,
+        dispatch: store.dispatch,
+      });
+
+      expect(selectors.selectCurrentState(store.getState())).toEqual(
+        states.GREEN,
+      );
+
+      // We should really be using a mock timer so we can run this test instantly
+      await wait(6000);
+      expect(selectors.selectCurrentState(store.getState())).toEqual(
+        states.YELLOW,
+      );
+    },
+    7000,
+  );
 });
