8ff0eebc5006ee88ad233ff0de90c7a57881060b
Karl O'Keeffe <github@monket.net>
2018-09-21 15:34:10 +0100

    Add a second unit test

--- redux-saga-state-machine/src/index.test.ts
+++ redux-saga-state-machine/src/index.test.ts
@@ -3,42 +3,87 @@ import { runSaga } from 'redux-saga';
 import { createStateMachineSaga } from './index';
 
 describe('createStateMachineSaga', () => {
+  const action1 = jest.fn();
+  const action2 = jest.fn();
+
   let clock: lolex.Clock;
+  let state: any;
+  let callbacks: Array<(action: any) => void>;
+  let getState: any;
+  let dispatch: any;
+  let setState: any;
+  let selectState: any;
+  let subscribe: any;
   beforeEach(() => {
     clock = lolex.install();
+
+    state = {
+      machineState: null,
+    };
+
+    callbacks = [];
+    getState = jest.fn(() => state);
+    dispatch = jest.fn((action) => {
+      for (const callback of callbacks) {
+        callback(action);
+      }
+    });
+    setState = jest.fn((machineState) => {
+      state.machineState = machineState;
+      return { type: 'SET_STATE', payload: machineState };
+    });
+    selectState = jest.fn(() => state.machineState);
+    subscribe = (callback: (action: any) => void) => {
+      callbacks = [...callbacks, callback];
+      return () => {
+        callbacks = callbacks.filter((c) => c !== callback);
+      };
+    };
   });
 
   afterEach(() => {
     clock.uninstall();
   });
 
+  it('transitions based on redux action', () => {
+    const stateMachine = {
+      key: 'test-state-machine',
+      // debug: true,
+      setState,
+      selectState,
+      initial: 'APP',
+      states: {
+        APP: {
+          on: {
+            play: 'PLAYER',
+          },
+        },
+        PLAYER: {},
+      },
+    };
+
+    const saga = createStateMachineSaga(stateMachine);
+
+    runSaga(
+      {
+        getState,
+        dispatch,
+        subscribe,
+      },
+      saga,
+      {
+        getState,
+        dispatch,
+      },
+    );
+
+    const playAction = { type: 'play' };
+    dispatch(playAction);
+
+    expect(state.machineState).toEqual('PLAYER');
+  });
+
   it('runs actions even when the state remains unchanged', () => {
-    const state = {
-      machineState: null,
-    };
-
-    let callbacks: Array<(action: any) => void> = [];
-    const getState = jest.fn(() => state);
-    const dispatch = jest.fn((action) => {
-      for (const callback of callbacks) {
-        callback(action);
-      }
-    });
-    const setState = jest.fn((machineState) => {
-      state.machineState = machineState;
-      return { type: 'SET_STATE', payload: machineState };
-    });
-    const selectState = jest.fn(() => state.machineState);
-    const subscribe = (callback: (action: any) => void) => {
-      callbacks = [...callbacks, callback];
-      return () => {
-        callbacks = callbacks.filter((c) => c !== callback);
-      };
-    };
-
-    const action1 = jest.fn();
-    const action2 = jest.fn();
-
     const stateMachine = {
       key: 'test-state-machine',
       // debug: true,
@@ -51,7 +96,6 @@ describe('createStateMachineSaga', () => {
             play: [{ target: 'APP', actions: [action1, action2] }],
           },
         },
-        PLAYER: {},
       },
     };
 
