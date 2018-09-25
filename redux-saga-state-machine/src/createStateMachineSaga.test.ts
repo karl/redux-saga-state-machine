b477f8e77b00a83d43d44730202f525db51bcc2b
Karl O'Keeffe <github@monket.net>
2018-09-25 21:25:21 +0100

    Share a single state machine across all tests

--- redux-saga-state-machine/src/createStateMachineSaga.test.ts
+++ redux-saga-state-machine/src/createStateMachineSaga.test.ts
@@ -1,19 +1,11 @@
 import { delay } from 'redux-saga';
 import { cancelled } from 'redux-saga/effects';
-import { createStateMachineSaga } from '.';
+import { createStateMachineSaga } from './createStateMachineSaga';
 import { createHarness } from './testing/createHarness';
 
 describe('createStateMachineSaga', () => {
-  const action1 = jest.fn();
-  const action2 = jest.fn();
-  const activity1Inner = jest.fn();
-  const activity1 = function*(action: any): any {
-    activity1Inner(action);
-  };
-  const onEntryApp = jest.fn();
-
   // Use this for debugging as it fully expands objects (the default console.log
-  // leave sub objects opaque).
+  // leaves sub objects opaque).
   // @ts-ignore
   const emit = (emitted) => {
     const { key, type, label, ...details } = emitted;
@@ -21,71 +13,25 @@ describe('createStateMachineSaga', () => {
     console.log(key, label, JSON.stringify(details, null, 2));
   };
 
-  let harness: any;
+  let harness;
+  let action1;
+  let action2;
+  let activityStart;
+  let onEntryApp;
+  let activityCancel;
+  let activity;
+  let stateMachine;
   beforeEach(() => {
     harness = createHarness();
-  });
 
-  afterEach(() => {
-    harness.clock.uninstall();
-  });
-
-  it('gets initial state from description', () => {
-    const stateMachine = {
-      key: 'test-state-machine',
-      // debug: true,
-      setState: harness.setState,
-      selectState: harness.selectState,
-      initial: 'APP',
-      states: {
-        APP: {
-          on: {
-            play: 'PLAYER',
-          },
-        },
-        PLAYER: {},
-      },
-    };
-
-    const saga = createStateMachineSaga(stateMachine);
-
-    harness.run(saga);
-
-    expect(harness.state.machineState).toEqual('APP');
-  });
-
-  it('run activities and onEntry for initial state', () => {
-    const stateMachine = {
-      key: 'test-state-machine',
-      // debug: true,
-      setState: harness.setState,
-      selectState: harness.selectState,
-      initial: 'APP',
-      states: {
-        APP: {
-          onEntry: [onEntryApp],
-          activities: [activity1],
-          on: {
-            play: 'PLAYER',
-          },
-        },
-        PLAYER: {},
-      },
-    };
-
-    const saga = createStateMachineSaga(stateMachine);
-
-    harness.run(saga);
-
-    // Note: Initial activities and onEntry are passed an empty action object (no type field!)
-    expect(onEntryApp).toHaveBeenCalledWith(harness.firstArg, {});
-    expect(activity1Inner).toHaveBeenCalledWith({});
-  });
-
-  it('cancels initial state activities when transitioning to a new state', () => {
-    const activityCancel = jest.fn();
-    const activity = function*() {
+    action1 = jest.fn();
+    action2 = jest.fn();
+    activityStart = jest.fn();
+    onEntryApp = jest.fn();
+    activityCancel = jest.fn();
+    activity = function*(action) {
       try {
+        activityStart(action);
         yield delay(5000);
       } finally {
         if (yield cancelled()) {
@@ -94,7 +40,7 @@ describe('createStateMachineSaga', () => {
       }
     };
 
-    const stateMachine = {
+    stateMachine = {
       key: 'test-state-machine',
       // debug: true,
       setState: harness.setState,
@@ -106,12 +52,39 @@ describe('createStateMachineSaga', () => {
           activities: [activity],
           on: {
             play: 'PLAYER',
+            sameInternal: [
+              { target: 'APP', internal: true, actions: [action1, action2] },
+            ],
           },
         },
         PLAYER: {},
       },
     };
+  });
 
+  afterEach(() => {
+    harness.clock.uninstall();
+  });
+
+  it('gets initial state from description', () => {
+    const saga = createStateMachineSaga(stateMachine);
+
+    harness.run(saga);
+
+    expect(harness.state.machineState).toEqual('APP');
+  });
+
+  it('run activities and onEntry for initial state', () => {
+    const saga = createStateMachineSaga(stateMachine);
+
+    harness.run(saga);
+
+    // Note: Initial activities and onEntry are passed an empty action object (no type field!)
+    expect(onEntryApp).toHaveBeenCalledWith(harness.firstArg, {});
+    expect(activityStart).toHaveBeenCalledWith({});
+  });
+
+  it('cancels initial state activities when transitioning to a new state', () => {
     const saga = createStateMachineSaga(stateMachine);
 
     harness.run(saga);
@@ -122,63 +95,30 @@ describe('createStateMachineSaga', () => {
     expect(activityCancel).toHaveBeenCalled();
   });
 
-  it('when internal activities remain running when transitioning to the same state', () => {
-    const activityCancel = jest.fn();
-    const activity = function*() {
-      try {
-        yield delay(5000);
-      } finally {
-        if (yield cancelled()) {
-          activityCancel();
-        }
-      }
-    };
-
-    const stateMachine = {
-      key: 'test-state-machine',
-      // debug: true,
-      setState: harness.setState,
-      selectState: harness.selectState,
-      initial: 'APP',
-      states: {
-        APP: {
-          onEntry: [onEntryApp],
-          activities: [activity],
-          on: {
-            play: [{ target: 'APP', internal: true }],
-          },
-        },
-        PLAYER: {},
-      },
-    };
-
+  it('when transition is internal activities remain running when transitioning to the same state', () => {
     const saga = createStateMachineSaga(stateMachine);
 
     harness.run(saga);
 
-    const playAction = { type: 'play' };
-    harness.dispatch(playAction);
+    const internalAction = { type: 'sameInternal' };
+    harness.dispatch(internalAction);
 
     expect(activityCancel).not.toHaveBeenCalled();
   });
 
-  it('transitions based on redux action', () => {
-    const stateMachine = {
-      key: 'test-state-machine',
-      // debug: true,
-      setState: harness.setState,
-      selectState: harness.selectState,
-      initial: 'APP',
-      states: {
-        APP: {
-          on: {
-            play: 'PLAYER',
-          },
-        },
-        PLAYER: {},
-      },
-    };
+  it('runs actions even when the state remains unchanged', () => {
+    const saga = createStateMachineSaga(stateMachine);
 
+    harness.run(saga);
+
+    const internalAction = { type: 'sameInternal' };
+    harness.dispatch(internalAction);
+
+    expect(action1).toHaveBeenCalledWith(harness.firstArg, internalAction);
+    expect(action2).toHaveBeenCalledWith(harness.firstArg, internalAction);
+  });
+
+  it('transitions based on redux action', () => {
     const saga = createStateMachineSaga(stateMachine);
 
     harness.run(saga);
@@ -187,32 +127,5 @@ describe('createStateMachineSaga', () => {
     harness.dispatch(playAction);
 
     expect(harness.state.machineState).toEqual('PLAYER');
-  });
-
-  it('runs actions even when the state remains unchanged', () => {
-    const stateMachine = {
-      key: 'test-state-machine',
-      // debug: true,
-      setState: harness.setState,
-      selectState: harness.selectState,
-      initial: 'APP',
-      states: {
-        APP: {
-          on: {
-            play: [{ target: 'APP', actions: [action1, action2] }],
-          },
-        },
-      },
-    };
-
-    const saga = createStateMachineSaga(stateMachine);
-
-    harness.run(saga);
-
-    const playAction = { type: 'play' };
-    harness.dispatch(playAction);
-
-    expect(action1).toHaveBeenCalledWith(harness.firstArg, playAction);
-    expect(action2).toHaveBeenCalledWith(harness.firstArg, playAction);
   });
 });
