111c0dc249367f4a9ebb12cc320ac59081de4847
Karl O'Keeffe <github@monket.net>
2018-09-27 16:48:59 +0100

    More tests

--- redux-saga-state-machine/src/createStateMachineSaga.test.ts
+++ redux-saga-state-machine/src/createStateMachineSaga.test.ts
@@ -13,15 +13,21 @@ describe('createStateMachineSaga', () => {
     console.log(key, label, JSON.stringify(details, null, 2));
   };
 
-  const play = () => ({ type: 'play' });
-  const sameInternal = () => ({ type: 'sameInternal' });
+  const actions = {
+    play: () => ({ type: 'play' }),
+    playInternal: () => ({ type: 'playInternal' }),
+    sameInternal: () => ({ type: 'sameInternal' }),
+    unknown: () => ({ type: 'unknown' }),
+  };
 
   let harness;
   let action1;
   let action2;
-  let activityStart;
+  let activityStarted;
   let onEntryApp;
-  let activityCancel;
+  let onExitApp;
+  let activityCancelled;
+  let activityComplete;
   let activity;
   let stateMachine;
   let saga;
@@ -30,16 +36,19 @@ describe('createStateMachineSaga', () => {
 
     action1 = jest.fn();
     action2 = jest.fn();
-    activityStart = jest.fn();
+    activityStarted = jest.fn();
     onEntryApp = jest.fn();
-    activityCancel = jest.fn();
+    onExitApp = jest.fn();
+    activityCancelled = jest.fn();
+    activityComplete = jest.fn();
     activity = function*(action) {
       try {
-        activityStart(action);
+        activityStarted(action);
         yield delay(5000);
+        activityComplete();
       } finally {
         if (yield cancelled()) {
-          activityCancel();
+          activityCancelled();
         }
       }
     };
@@ -53,9 +62,11 @@ describe('createStateMachineSaga', () => {
       states: {
         APP: {
           onEntry: [onEntryApp],
+          onExit: [onExitApp],
           activities: [activity],
           on: {
             play: 'PLAYER',
+            playInternal: [{ target: 'PLAYER', internal: true }],
             sameInternal: [
               { target: 'APP', internal: true, actions: [action1, action2] },
             ],
@@ -81,31 +92,77 @@ describe('createStateMachineSaga', () => {
     harness.run(saga);
     // Note: Initial activities and onEntry are passed an empty action object (no type field!)
     expect(onEntryApp).toHaveBeenCalledWith(harness.firstArg, {});
-    expect(activityStart).toHaveBeenCalledWith({});
+    expect(activityStarted).toHaveBeenCalledWith({});
   });
 
-  it('cancels initial state activities when transitioning to a new state', () => {
+  it('cancels activities when transitioning to a new state', () => {
     harness.run(saga);
-    harness.dispatch(play());
-    expect(activityCancel).toHaveBeenCalled();
+    harness.dispatch(actions.play());
+    expect(activityCancelled).toHaveBeenCalled();
+  });
+
+  it('if an activity runs to completion it does not get cancelled when transitioning to a new state', () => {
+    harness.run(saga);
+    harness.clock.tick(5001);
+    expect(activityComplete).toHaveBeenCalled();
+    harness.dispatch(actions.play());
+    expect(activityCancelled).not.toHaveBeenCalled();
   });
 
   it('when transition is internal activities remain running when transitioning to the same state', () => {
     harness.run(saga);
-    harness.dispatch(sameInternal());
-    expect(activityCancel).not.toHaveBeenCalled();
+    harness.dispatch(actions.sameInternal());
+    expect(activityCancelled).not.toHaveBeenCalled();
+  });
+
+  it('when transition is internal onEntry and onExit is not run when transitioning to the same state', () => {
+    harness.run(saga);
+    jest.clearAllMocks();
+    harness.dispatch(actions.sameInternal());
+    expect(onEntryApp).not.toHaveBeenCalled();
+    expect(onExitApp).not.toHaveBeenCalled();
   });
 
   it('runs actions even when the state remains unchanged', () => {
     harness.run(saga);
-    harness.dispatch(sameInternal());
-    expect(action1).toHaveBeenCalledWith(harness.firstArg, sameInternal());
-    expect(action2).toHaveBeenCalledWith(harness.firstArg, sameInternal());
+    harness.dispatch(actions.sameInternal());
+    expect(action1).toHaveBeenCalledWith(
+      harness.firstArg,
+      actions.sameInternal(),
+    );
+    expect(action2).toHaveBeenCalledWith(
+      harness.firstArg,
+      actions.sameInternal(),
+    );
   });
 
   it('transitions based on redux action', () => {
     harness.run(saga);
-    harness.dispatch(play());
+    harness.dispatch(actions.play());
     expect(harness.state.machineState).toEqual('PLAYER');
+  });
+
+  it('when leaving a state runs onExit', () => {
+    harness.run(saga);
+    harness.dispatch(actions.play());
+    expect(onExitApp).toHaveBeenCalledWith(harness.firstArg, actions.play());
+  });
+
+  it('when leaving a state, if the transition is marked as internal but moves to a different state,  runs onExit', () => {
+    harness.run(saga);
+    harness.dispatch(actions.playInternal());
+    expect(onExitApp).toHaveBeenCalledWith(
+      harness.firstArg,
+      actions.playInternal(),
+    );
+  });
+
+  it('when there is a redux action with no transitions, state is unchanged and activities keep running', () => {
+    harness.run(saga);
+    jest.clearAllMocks();
+    harness.dispatch(actions.unknown());
+    expect(onEntryApp).not.toHaveBeenCalled();
+    expect(onExitApp).not.toHaveBeenCalled();
+    expect(activityCancelled).not.toHaveBeenCalled();
   });
 });
