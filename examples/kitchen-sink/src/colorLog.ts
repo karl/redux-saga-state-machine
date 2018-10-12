04f2c1c3136ce91fe4104ff6d0283be81104a160
Karl O'Keeffe <github@monket.net>
2018-10-12 22:28:47 +0100

    no-deploy: Kitchen sink on code sandbox (#177)
    * Use Parcel to build kitchen sink example
    
    * Fix build of examples so they can work in Docusaurus
    
    * Start getting kitchen sink to work on Code Sandbox

--- examples/kitchen-sink/src/colorLog.ts
+++
@@ -1,8 +0,0 @@
-export const colorLog = (color: string, label: string, ...data: any[]) => {
-  // tslint:disable-next-line:no-console
-  console.log(
-    `%c${label}`,
-    `background-color: ${color}; color: #000; padding: 2px 4px; font-weight: bold;`,
-    ...data,
-  );
-};
